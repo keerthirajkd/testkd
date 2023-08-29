e6b3cfc7f858290f5946a96f02c0a633f59282b1
keerthirajkd <140588114+keerthirajkd@users.noreply.github.com>
2023-08-30 00:35:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,32 +7,16 @@ function toggle() {
   if (active) {
     toggle.classList.add("active");
     text.innerHTML = "ON";
-
-/* pendo.track("TOGGLE_STATUS", {
- toggleState: "ON"
- });
-
- console.log("Pendo Track Event Called with status 'ON'")
+    console.log("Pendo Track Event Called with status 'ON'")
     
- } else {
- toggle.classList.remove("active");
- text.innerHTML = "OFF";
-
-  pendo.track("TOGGLE_STATUS", {
-    toggleState: "OFF"
-    });
-
+  } else {
+    toggle.classList.remove("active");
+    text.innerHTML = "OFF";
+pendo.showGuideById('s3HvZVlDql5rUXOdgjBaAobarXw')
     console.log("Pendo Track Event Called with status 'OFF'")
   }
-}*/
-    let toggleState="OFF";
-if(toggleState == "OFF"){
-  pendo.showGuideById('s3HvZVlDql5rUXOdgjBaAobarXw')
-  console.log("Toggle is OFF")
 }
-else{
-  console.log("toggle is ON")
-}
+
 
 $('#exampleModal').on('shown.bs.modal', function () {
   // $('#myInput').trigger('focus')

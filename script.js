d7f412a8258ec48210d3d10edb6d3a733504331f
keerthirajkd <140588114+keerthirajkd@users.noreply.github.com>
2023-10-13 20:58:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-let active = false;
-
-function toggle() {
-  let toggle = document.querySelector(".toggle");
-  let text = document.querySelector(".text");
-  active = !active;
-  if (active) {
-    toggle.classList.add("active");
-    text.innerHTML = "ON";
-    guide.hide({stayHidden: true});
-
-    console.log("Pendo Track Event Called with status 'ON'")
-    
-  } else {
-    toggle.classList.remove("active");
-    text.innerHTML = "OFF";
-pendo.showGuideById('s3HvZVlDql5rUXOdgjBaAobarXw')
-    console.log("Pendo Track Event Called with status 'OFF'")
-  }
+if (screen.width < 960) {
+    pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6')
 }
+else {
+
+    alert('More than 960');
+}
+//let active = false;
+
+// function toggle() {
+//   let toggle = document.querySelector(".toggle");
+//   let text = document.querySelector(".text");
+//   active = !active;
+//   if (active) {
+//     toggle.classList.add("active");
+//     text.innerHTML = "ON";
+//     guide.hide({stayHidden: true});
+
+//     console.log("Pendo Track Event Called with status 'ON'")
+    
+//   } else {
+//     toggle.classList.remove("active");
+//     text.innerHTML = "OFF";
+// pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6')
+//     console.log("Pendo Track Event Called with status 'OFF'")
+//   }
+// }
 
 
-$('#exampleModal').on('shown.bs.modal', function () {
+//$('#exampleModal').on('shown.bs.modal', function () {
   // $('#myInput').trigger('focus')
 })

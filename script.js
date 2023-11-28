b5a1dccac97f1e8f8f536af8a423dd52b266ada5
keerthirajkd <140588114+keerthirajkd@users.noreply.github.com>
2023-11-28 18:56:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-setTimeout(() => {
+// setTimeout(() => {
     
-    if (screen.width > 960) {
-    pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6-Rhgww')
-}
-else {
-
-    alert('More than 960');
-}
-    }, 3000);
-
-
-//let active = false;
-
-// function toggle() {
-//   let toggle = document.querySelector(".toggle");
-//   let text = document.querySelector(".text");
-//   active = !active;
-//   if (active) {
-//     toggle.classList.add("active");
-//     text.innerHTML = "ON";
-//     guide.hide({stayHidden: true});
-
-//     console.log("Pendo Track Event Called with status 'ON'")
-    
-//   } else {
-//     toggle.classList.remove("active");
-//     text.innerHTML = "OFF";
-// pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6')
-//     console.log("Pendo Track Event Called with status 'OFF'")
-//   }
+//     if (screen.width > 960) {
+//     pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6-Rhgww')
 // }
+// else {
+
+//     alert('More than 960');
+// }
+//     }, 3000);
+
+
+let active = false;
+
+function toggle() {
+  let toggle = document.querySelector(".toggle");
+  let text = document.querySelector(".text");
+  active = !active;
+  if (active) {
+    toggle.classList.add("active");
+    text.innerHTML = "ON";
+    guide.hide({stayHidden: true});
+
+    console.log("Pendo Track Event Called with status 'ON'")
+    
+  } else {
+    toggle.classList.remove("active");
+    text.innerHTML = "OFF";
+pendo.showGuideById('RxYqXOlTbOSfYtvUDRiL6')
+    console.log("Pendo Track Event Called with status 'OFF'")
+  }
+}
 
 
 //$('#exampleModal').on('shown.bs.modal', function () {

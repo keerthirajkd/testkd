ba7586993c4e4085c3371f9ce0859a3c86946e07
keerthirajkd <140588114+keerthirajkd@users.noreply.github.com>
2023-11-28 19:02:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ function toggle() {
     toggle.classList.remove("active");
     text.innerHTML = "OFF";
 pendo.showGuideById('A5P1IbQJ91lcT7ytAv2AJSAMNrM')
+	  
+            pendo.track("toggle", {
+                      name: "ON/OFF"
+            });
+            alert("Toggle changed");
+          }
     console.log("Pendo Track Event Called with status 'OFF'")
   }
 }

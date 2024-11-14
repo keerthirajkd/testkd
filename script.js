8e297eb2cb299aafea48b714b2d4e82520c3513f
Prajwalnecklaje <150286363+Prajwalnecklaje@users.noreply.github.com>
2024-11-14 22:41:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -156,12 +156,3 @@ let toglisub = function removeElementsByClass(Cls){
         elements[0].parentNode.removeChild(elements[0]);
     };
 }
-
-<script type="text/javascript" async >
-    function genericSocialShare(url){
-       window.open(url,'sharer','toolbar=0,status=0,width=648,height=395');
-       return true;
-    }
-</script>
-
-<a href="javascript:void(0)" onclick="javascript:genericSocialShare('https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=youremail@gmail.com&su=yoursubject&body=my-text&ui=2&tf=1&pli=1')"></a></li>
